86177afa7caf0124aed9e4ec951aee74cdd84865
Ayu Mutia <101488449+fylnnx@users.noreply.github.com>
2025-01-04 20:19:36 +0800

    Update extract.js

--- netlify/functions/extract.js
+++ netlify/functions/extract.js
@@ -1,15 +1,15 @@
 const fs = require('fs').promises;
 const path = require('path');
-const { encryptECB, decryptECB } = require('../ECB');
-const { embed, extract } = require('../LCG');
+const { encryptECB, decryptECB } = require('../../ECB');
+const { embed, extract } = require('../../LCG');
 
 exports.handler = async function (event, context) {
   const formData = new URLSearchParams(await event.body);
   const key = formData.get('key');
   const stegoAudioPath = formData.get('stegoAudio');
 
-  const extractedPath = path.join(__dirname, '../uploads', 'extracted.txt');
-  const decryptedPath = path.join(__dirname, '../uploads', 'decrypted.txt');
+  const extractedPath = path.join(__dirname, '../../uploads', 'extracted.txt');
+  const decryptedPath = path.join(__dirname, '../../uploads', 'decrypted.txt');
 
   try {
     const cipherText = extract(stegoAudioPath, key);
