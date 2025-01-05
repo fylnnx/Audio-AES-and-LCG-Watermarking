1457b9330f94e33a0b6ef59d9a12c11221b990e5
Ayu Mutia <101488449+fylnnx@users.noreply.github.com>
2025-01-05 13:04:51 +0800

    Update extract.js

--- netlify/functions/extract.js
+++ netlify/functions/extract.js
@@ -1,35 +1,31 @@
 const fs = require('fs').promises;
 const path = require('path');
-const { extract } = require('../../LCG');
 const { decryptECB } = require('../../ECB');
+const { extract } = require('../../LCG');
 
 exports.handler = async (event, context) => {
-  if (event.httpMethod === 'POST') {
-    try {
-      const { key, stegoAudioFile } = JSON.parse(event.body);
+  const { stegoAudio, key } = JSON.parse(event.body);
 
-      const extractedPath = path.join(__dirname, 'uploads', 'extracted.txt');
-      const decryptedPath = path.join(__dirname, 'uploads', 'decrypted.txt');
+  const stegoAudioData = Buffer.from(stegoAudio, 'base64');
+  const extractedPath = path.join(__dirname, 'uploads', 'extracted.txt');
+  const decryptedPath = path.join(__dirname, 'uploads', 'decrypted.txt');
 
-      const cipherText = extract(stegoAudioFile, key);
-      await fs.writeFile(extractedPath, cipherText, 'utf8');
-      await decryptECB(extractedPath, decryptedPath, key);
+  try {
+    const cipherText = extract(stegoAudioData, key);
+    await fs.writeFile(extractedPath, cipherText, 'utf8');
+    await decryptECB(extractedPath, decryptedPath, key);
 
-      const plainText = await fs.readFile(decryptedPath, 'utf8');
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ cipherText, plainText }),
-      };
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: 'Error extracting audio' }),
-      };
-    }
+    const plainText = await fs.readFile(decryptedPath, 'utf8');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ cipherText, plainText })
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error extracting audio' })
+    };
   }
-
-  return {
-    statusCode: 405,
-    body: JSON.stringify({ error: 'Method Not Allowed' }),
-  };
 };
