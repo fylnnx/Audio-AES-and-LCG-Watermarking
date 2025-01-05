f2e958e828f983f501e4f623701816dce37c1fcb
Ayu Mutia <101488449+fylnnx@users.noreply.github.com>
2025-01-05 17:28:23 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
-import { calculateMSE, calculatePSNR, readAudioFile, testPSNRandMSE } from './uji';
-import { encryptECB, decryptECB } from './ECB';
-import { embed, extract } from './LCG';
+import { calculateMSE, calculatePSNR, readAudioFile, testPSNRandMSE } from 'uji.js';
+import { encryptECB, decryptECB } from 'ECB.js';
+import { embed, extract } from 'LCG.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const navButtons = document.querySelectorAll('[data-page]');
