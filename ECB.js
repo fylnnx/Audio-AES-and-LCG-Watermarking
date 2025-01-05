79950eeb00af0ddde85328ebaa466193d18f1734
Ayu Mutia <101488449+fylnnx@users.noreply.github.com>
2025-01-05 13:03:19 +0800

    Update ECB.js

--- ECB.js
+++ ECB.js
@@ -1,98 +1,44 @@
 const { encrypt, decrypt } = require('./AES');
 
-const fs = require('fs');
+// Fungsi untuk mengenkripsi data
+function encryptECB(inputData, key) {
+    // Pisahkan inputData menjadi blok 16 karakter
+    const blockSize = 16;
+    let blocks = [];
 
-// Fungsi membaca file dan memisahkan menjadi blok 16 karakter
-function readFileAndSplit(filePath, blockSize = 16) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                reject('Gagal membaca file: ' + err);
-                return;
-            }
-
-            // Tambahkan padding jika panjang data bukan kelipatan 16
-            if (data.length % blockSize !== 0) {
-                const paddingLength = blockSize - (data.length % blockSize);
-                data += ' '.repeat(paddingLength); // Tambahkan spasi sebagai padding
-            }
-
-            // Pisahkan data menjadi blok-blok 16 karakter
-            const blocks = [];
-            for (let i = 0; i < data.length; i += blockSize) {
-                blocks.push(data.slice(i, i + blockSize));
-            }
-
-            console.log('blocks:',blocks)
-
-            resolve(blocks);
-        });
-    });
-}
-
-//write file
-function writeFile(filePath, content) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(filePath, content, 'utf8', (err) => {
-            if (err) {
-                reject('Failed to write file ' + err);
-                return;
-            }
-            resolve('File successfully written at ' + filePath);
-        });
-    });
-}
-
-//encrypt ecb
-async function encryptECB(inputFile, outputFile, key) {
-    try {
-        const blocks = await readFileAndSplit(inputFile);
-        console.log('Blok data asli:', blocks);
-
-        const encryptedBlocks = blocks.map(block => encrypt(block, key));
-        console.log('Blok terenkripsi:', encryptedBlocks);
-
-        const encryptedString = encryptedBlocks.join('');
-
-        const result = await writeFile(outputFile, encryptedString);
-        console.log(result);
-    } catch (err) {
-        console.error(err);
+    // Tambahkan padding jika panjang data bukan kelipatan 16
+    if (inputData.length % blockSize !== 0) {
+        const paddingLength = blockSize - (inputData.length % blockSize);
+        inputData += ' '.repeat(paddingLength); // Tambahkan spasi sebagai padding
     }
-}
 
-async function decryptECB(inputFile, outputFile, key) {
-    try {
-        // Baca file yang telah terenkripsi
-        const blocks = await readFileAndSplit(inputFile);
-        console.log('Blok data terenkripsi:', blocks);
-
-        // Dekripsi setiap blok
-        const decryptedBlocks = blocks.map(block => decrypt(block, key));
-        console.log('Blok terdekripsi:', decryptedBlocks);
-
-        // Gabungkan blok-blok terdekripsi menjadi string
-        const decryptedString = decryptedBlocks.join('').trimEnd(); // Hapus padding di akhir
-
-        // Simpan hasil ke file
-        const result = await writeFile(outputFile, decryptedString);
-        console.log(result);
-    } catch (err) {
-        console.error(err);
+    // Pisahkan data menjadi blok-blok 16 karakter
+    for (let i = 0; i < inputData.length; i += blockSize) {
+        blocks.push(inputData.slice(i, i + blockSize));
     }
+
+    // Enkripsi setiap blok
+    const encryptedBlocks = blocks.map(block => encrypt(block, key));
+    const encryptedString = encryptedBlocks.join('');
+
+    return encryptedString;
 }
 
-// Contoh penggunaan
-// const inputFile = 'file.txt';
-// const encryptedFile = 'encrypted.txt';
-// const decryptedFile = 'decrypted.txt';
-// const key = 'my_secret_key123'; // Kunci AES (harus panjang 16 karakter)
+// Fungsi untuk mendekripsi data
+function decryptECB(encryptedData, key) {
+    // Pisahkan data terenkripsi menjadi blok 16 karakter
+    const blockSize = 16;
+    let blocks = [];
 
-// // Enkripsi
-// encryptECB(inputFile, encryptedFile, key);
+    for (let i = 0; i < encryptedData.length; i += blockSize) {
+        blocks.push(encryptedData.slice(i, i + blockSize));
+    }
 
-// // Dekripsi
-// decryptECB(encryptedFile, decryptedFile, key);
+    // Dekripsi setiap blok
+    const decryptedBlocks = blocks.map(block => decrypt(block, key));
+    const decryptedString = decryptedBlocks.join('').trimEnd(); // Hapus padding di akhir
 
-module.exports = {encryptECB, decryptECB};
+    return decryptedString;
+}
 
+module.exports = { encryptECB, decryptECB };
