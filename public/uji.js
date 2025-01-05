b3497481912c9d2ecf6f2bd9179d1c9dccce27fc
Ayu Mutia <101488449+fylnnx@users.noreply.github.com>
2025-01-05 18:18:10 +0800

    Update uji.js

--- public/uji.js
+++ public/uji.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
-
+// Hapus require dan module.exports, ubah ke ekspor ES Modules
 // Fungsi untuk menghitung MSE
-function calculateMSE(originalSamples, modifiedSamples) {
+export function calculateMSE(originalSamples, modifiedSamples) {
     let sum = 0;
     for (let i = 0; i < originalSamples.length; i++) {
         sum += Math.pow(originalSamples[i] - modifiedSamples[i], 2);
@@ -10,34 +9,27 @@ function calculateMSE(originalSamples, modifiedSamples) {
 }
 
 // Fungsi untuk menghitung PSNR
-function calculatePSNR(mse) {
+export function calculatePSNR(mse) {
     const MAX_I = 32767; // Maksimum nilai untuk 16-bit audio
     return 10 * Math.log10(Math.pow(MAX_I, 2) / mse);
 }
 
 // Fungsi untuk membaca audio sebagai array of Int16
-function readAudioFile(fileName) {
+export function readAudioFile(fileName) {
     const audioBuffer = fs.readFileSync(fileName);
     const audioSamples = new Int16Array(audioBuffer.buffer, audioBuffer.byteOffset, audioBuffer.length / Int16Array.BYTES_PER_ELEMENT);
     return audioSamples;
 }
 
 // Uji PSNR dan MSE
-function testPSNRandMSE(originalFile, modifiedFile) {
+export function testPSNRandMSE(originalFile, modifiedFile) {
     const originalSamples = readAudioFile(originalFile);
     const modifiedSamples = readAudioFile(modifiedFile);
 
     const mse = calculateMSE(originalSamples, modifiedSamples);
     const psnr = calculatePSNR(mse);
     
-    console.log(mse,psnr)
+    console.log(mse, psnr);
 
     return { mse, psnr };
 }
-
-// const originalFile = 'audio.wav'
-// const modifiedFile = 'stego_audio.wav'
-// testPSNRandMSE(originalFile, modifiedFile)
-
-
-module.exports = { calculateMSE, calculatePSNR, readAudioFile, testPSNRandMSE };
